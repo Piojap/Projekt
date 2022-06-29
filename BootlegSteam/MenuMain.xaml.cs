f5e4008597953d626527e112e0523ace43d971db
PC <PC@DESKTOP-MCKC9BO>
2022-06-29 21:35:15 +0200

    29.06.22 edit

---
+++ BootlegSteam/MenuMain.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BootlegSteam
+{
+    /// <summary>
+    /// Interaction logic for MenuMain.xaml
+    /// </summary>
+    public partial class MenuMain : Window
+    {
+        public MenuMain()
+        {
+            InitializeComponent();
+        }
+
+        private void clkmenuplayer(object sender, RoutedEventArgs e)
+        {
+            MenuPlayer open = new MenuPlayer();
+            this.Visibility = Visibility.Hidden;
+            open.Show();
+        }
+
+        private void clkmenugame(object sender, RoutedEventArgs e)
+        {
+            MenuGame open = new MenuGame();
+            this.Visibility = Visibility.Hidden;
+            open.Show();
+        }
+
+        private void clkmenuicon(object sender, RoutedEventArgs e)
+        {
+            MenuIcon open = new MenuIcon();
+            this.Visibility = Visibility.Hidden;
+            open.Show();
+        }
+
+        private void clkmenugenre(object sender, RoutedEventArgs e)
+        {
+            MenuGenre open = new MenuGenre();
+            this.Visibility = Visibility.Hidden;
+            open.Show();
+        }
+
+        private void clkclose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void clkmaximize(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+        }
+
+        private void clkminimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void dragwindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
---
+++ BootlegSteam/MenuPlayer.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BootlegSteam
+{
+    /// <summary>
+    /// Interaction logic for MenuPlayer.xaml
+    /// </summary>
+    public partial class MenuPlayer : Window
+    {
+        public MenuPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void clkclose(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void clkmaximize(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+        }
+
+        private void clkminimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void dragwindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
